65082b73c8675bc9cef2426c95a1601d727967ff
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-01-09 18:13:37 -0600

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,6 +8,22 @@ Created on Thu Jan  9 17:22:18 2025
 import streamlit as st
 import pandas as pd
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Título de la aplicación
 page = st.sidebar.radio("Navegar por", ["Formulario", "Comentarios"])
 
